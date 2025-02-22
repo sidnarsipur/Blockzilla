e1b5643932b71a61b5bf3defb0c896daffa2fc8f
Henry Liu <codicate@gmail.com>
2025-02-22 13:29:56 -0500

    router install

--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-
+import axios from 'axios';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 const queryClient = new QueryClient();
+
+// axios.defaults.baseURL = 'https://api.example.com';
+// axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
