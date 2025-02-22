67c8082c0d22e4960e73cada7ade3c34147dbcc2
sidnarsipur <72701664+sidnarsipur@users.noreply.github.com>
2025-02-22 18:19:51 -0500

    change cursor color

--- admin/src/pages/Stat.tsx
+++ admin/src/pages/Stat.tsx
@@ -77,7 +77,7 @@ export default function Stat() {
                                         onChange={() => handleToggle(rule)}
                                         className="peer sr-only"
                                     />
-                                    <div className="peer h-5 w-9 rounded-full bg-gray-200 after:absolute after:left-[2px] after:top-[2px] after:h-4 after:w-4 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-green-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-blue-800"></div>
+                                    <div className="peer h-5 w-9 rounded-full bg-gray-200 after:absolute after:left-[2px] after:top-[2px] after:h-4 after:w-4 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-blue-800"></div>
                                 </label>
                             </div>
                             <div className="flex space-x-2">
