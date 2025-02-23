158b7a08830074bd766e0e778154c5ad327ed804
Henry Liu <codicate@gmail.com>
2025-02-22 19:55:46 -0500

    new rule added

--- admin/src/components/ChatBubble.tsx
+++ admin/src/components/ChatBubble.tsx
@@ -1,5 +1,5 @@
 import { streamText, useAnimatedText } from '@/hooks/useAnimatedText';
-import { Chat } from '@/lib/types';
+import { Chat, Rule } from '@/lib/types';
 import { useEffect } from 'react';
 import { ThumbsUp, ThumbsDown, Download } from 'lucide-react';
 
@@ -26,19 +26,36 @@ export default function ChatBubble({ chat }: { chat: Chat }) {
             >
                 {chat.message}
             </div>
-            {!chat.isUser && (
-                <div className="ml-2 mt-2 flex gap-2 text-xs text-gray-400">
-                    <button className="hover:text-gray-200">
-                        <ThumbsUp size={14} />
-                    </button>
-                    <button className="hover:text-gray-200">
-                        <ThumbsDown size={14} />
-                    </button>
-                    <button className="hover:text-gray-200">
-                        <Download size={14} />
-                    </button>
-                </div>
-            )}
+            {!chat.isUser && <RuleBox rule={chat.rule} />}
+        </div>
+    );
+}
+
+function RuleBox({ rule }: { rule?: Rule }) {
+    return (
+        <div>
+            <div className="ml-2 mt-2 flex gap-2 text-xs text-gray-400">
+                <button className="hover:text-gray-200">
+                    <ThumbsUp size={14} />
+                </button>
+                <button className="hover:text-gray-200">
+                    <ThumbsDown size={14} />
+                </button>
+                <button className="hover:text-gray-200">
+                    <Download size={14} />
+                </button>
+            </div>
+            <p className="mb-2 mt-5 text-center text-gray-400">
+                New Rule Added
+            </p>
+            <div className="mx-6 rounded-lg border border-gray-200 bg-white px-5 py-3 shadow-md transition-shadow">
+                <h3 className="mb-1 text-lg font-semibold text-gray-800">
+                    {rule?.name}
+                </h3>
+                <p className="line-clamp-2 text-sm text-gray-500">
+                    {rule?.description}
+                </p>
+            </div>
         </div>
     );
 }
--- admin/src/lib/types.ts
+++ admin/src/lib/types.ts
@@ -1,4 +1,10 @@
 export interface Chat {
     message: string;
     isUser: boolean;
+    rule?: Rule;
+}
+
+export interface Rule {
+    name: string;
+    description: string;
 }
