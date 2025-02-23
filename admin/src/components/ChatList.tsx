ddbc57b90a33d0e816d869fd7887fba5c4979514
Henry Liu <codicate@gmail.com>
2025-02-22 20:09:21 -0500

    styling change

--- admin/src/components/ChatList.tsx
+++ admin/src/components/ChatList.tsx
@@ -1,9 +1,9 @@
-import { Chat } from '@/lib/types';
 import ChatBubble from './ChatBubble';
 import { DotLoader } from 'react-spinners';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import { useChatStore } from '@/store/chats';
 import IntroCard from './IntroCard';
+import { Settings2 } from 'lucide-react';
 
 interface Props {
     isPending: boolean;
@@ -21,16 +21,22 @@ export default function ChatList({ isPending }: Props) {
     if (chats.length === 0) return <IntroCard />;
 
     return (
-        <div className="scrollbar-hide flex flex-col gap-6 overflow-y-scroll p-6">
-            {chats.map((chat, idx) => (
-                <ChatBubble key={idx} chat={chat} />
-            ))}
-            {isPending && (
-                <div className="h-[40px] w-[40px]">
-                    <DotLoader size={30} color="#33ae5a" />
-                </div>
-            )}
-            <div ref={chatEndRef} />
-        </div>
+        <>
+            <div className="flex flex-row items-center justify-between px-6 py-4 text-lg font-medium text-gray-500">
+                <p>AI Blocker</p>
+                <Settings2 />
+            </div>
+            <div className="scrollbar-hide flex flex-col gap-6 overflow-y-scroll p-6 pt-0">
+                {chats.map((chat, idx) => (
+                    <ChatBubble key={idx} chat={chat} />
+                ))}
+                {isPending && (
+                    <div className="h-[40px] w-[40px]">
+                        <DotLoader size={30} color="#2ea654" />
+                    </div>
+                )}
+                <div ref={chatEndRef} />
+            </div>
+        </>
     );
 }
